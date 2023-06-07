a8b8f3c93c724b8651209f43c1ad640c600c620e
unknown <alerodase@gmail.com>
2023-06-07 09:06:56 +0200

    creacion boton compra

--- src/components/Evento/Evento.jsx
+++ src/components/Evento/Evento.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import "./Evento.css";
 import { Link } from "react-router-dom";
+import Button from '../Button/Button'
 
 const Evento = ({ evento }) => {
+
+  
+  const comprar = () => {
+    window.location.href = 'https://www.ticketmaster.es/';
+  };
+
   return (
     <div>
-      <h2>
-        <Link to={{ pathname: `/${evento._id}`, state: { evento } }}>EVENTO</Link>
-      </h2>
       <div>
-        <p>{evento._id}</p>
-        <p>{evento.title}</p>
+        {/* <p>{evento._id}</p> */}
+        <Link to={{ pathname: `/${evento._id}`, state: { evento } }}><h1>{evento.title}</h1></Link>
         <p>{evento.subtitle}</p>
         <p>{evento.content}</p>
         <p>{evento.user_creator}</p>
         <p>{evento.site}</p>
         <p>{evento.date_start}</p>
         <p>{evento.date_end}</p>
-        <p>{evento.url}</p>
-        <img src={evento.image}/>
+        <Button text="Comprar" type="medium" onClick={comprar}/>
+        <Link to={{ pathname: `/${evento._id}`, state: { evento } }}><img src={evento.image}/></Link>
         <p>{evento.genre}</p>
       </div>
     </div>
