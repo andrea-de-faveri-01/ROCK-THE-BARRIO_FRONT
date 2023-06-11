e6b449058d52169f1867b6dd63419bac2e58a771
Miguel <miguelaf71@hotmail.com>
2023-06-12 01:49:33 +0200

    error al añadir comentario

--- src/redux/comentarios/comentarios.reducer.js
+++ src/redux/comentarios/comentarios.reducer.js
@@ -1,27 +1,28 @@
 const INITIAL_STATE = {
   comentarios: [],
   loading: false,
+  error: null, // Agregamos una propiedad "error" al estado inicial
 };
+
 export const comentariosReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "LOADING":
-      return { ...state, loading: true };
+      return { ...state, loading: true, error: null };
 
     case "GET_COMENTARIOSBYEVENTO":
-      return { ...state, loading: false, comentarios: action.contenido };
+      return { ...state, loading: false, comentarios: action.contenido, error: null };
     
     case "ADD_COMENTARIO":
-      return {...state, loading: false, comentarios: [...state.comentarios, action.contenido]}
+      return {...state, loading: false, comentarios: [...state.comentarios, action.contenido], error: null }
 
     case "EDIT_COMENTARIO":
       return {
         ...state,
         loading: false,
         comentarios: state.comentarios.map((comentario) =>
-          comentario._id === action.contenido._id
-            ? action.contenido
-            : comentario
+          comentario._id === action.contenido._id ? action.contenido : comentario
         ),
+        error: null,
       };
 
     case "DELETE_COMENTARIO":
@@ -31,6 +32,7 @@ export const comentariosReducer = (state = INITIAL_STATE, action) => {
         comentarios: state.comentarios.filter(
           (comentario) => comentario._id !== action.contenido._id
         ),
+        error: null,
       };
       
     case "ERROR_COMENTARIOS":
@@ -39,6 +41,7 @@ export const comentariosReducer = (state = INITIAL_STATE, action) => {
         loading: false,
         error: action.contenido,
       };
+
     default:
       return state;
   }
