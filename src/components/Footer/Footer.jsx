071f40f1c0a77343929588893f8e6af37be2338c
andrea-de-faveri-01 <andreadefaveri01@gmail.com>
2023-06-12 18:50:31 +0100

    he eliminado los tooltip del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 import Button from "../Button/Button";
 import { GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
-import { Tooltip } from "@fluentui/react-components";
+
 
 
 
@@ -17,48 +17,43 @@ const scrollToTop = () => {
     return (
         <div className="footer-container">
       <div className="ant-icons">
-      <Tooltip content="GitHub FontEnd" relationship="description" >
+      {/* <p>GitHub FontEnd</p> */}
         <a href="https://github.com/andrea-de-faveri-01/ROCK-THE-BARRIO_FRONT"
         target="_blank" rel="noopener noreferrer"
         >
           <GithubOutlined className="ant-icon" />
         </a>
-        </Tooltip>
-        <Tooltip content="GitHub BackEnd" relationship="description">
+        {/* <p>GitHub BackEnd</p> */}
         <a href="https://github.com/GaleGizmo/ROCK_DE_BARRIO_BACKEND" 
         target="_blank" rel="noopener noreferrer"
         >
           <GithubOutlined className="ant-icon" />
         </a>
-        </Tooltip>
-        <Tooltip content="Miguel 🎸" relationship="description">
+        {/* <p>Miguel 🎸</p> */}
         <a href="https://www.linkedin.com/in/miguelabelleira-fsd/"
         target="_blank" rel="noopener noreferrer"
         >
           <LinkedinOutlined className="ant-icon" />
         </a>
-        </Tooltip>
-        <Tooltip content="Moha 🥁" relationship="description">
+        {/* <p>Moha 🥁</p> */}
         <a href="https://www.linkedin.com/in/mohamed-nour-abdulla-743587176/"
         target="_blank" rel="noopener noreferrer"
         >
           <LinkedinOutlined className="ant-icon" />
         </a>
-        </Tooltip>
-        <Tooltip content="Alex 🎷" relationship="description">
+        
+        {/* <p>Alex 🎷</p> */}
         <a href="https://www.linkedin.com/in/alejandro-rodriguez-asencio-full-stack-developer/"
         target="_blank" rel="noopener noreferrer"
         >
           <LinkedinOutlined className="ant-icon" />
         </a>
-        </Tooltip>
-        <Tooltip content="Andrea 🎺" relationship="description">
+        {/* <p>Andrea 🎺</p> */}
         <a href="https://www.linkedin.com/in/andrea-de-faveri-dev/"
         target="_blank" rel="noopener noreferrer"
         >
           <LinkedinOutlined className="ant-icon" />
         </a>
-        </Tooltip>
       </div>
       <div className="footer-btn-container">
           <Button className="footer-btn" text="Vuelve arriba" type="medium" onClick={scrollToTop}/>
