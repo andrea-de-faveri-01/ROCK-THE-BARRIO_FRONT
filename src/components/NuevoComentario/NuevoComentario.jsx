e33adbfcdf51518c094f864b141888198ef2caad
Abanur <mohamednourabdulla@outlook.es>
2023-06-13 11:02:20 +0200

    Se ha modificado el NuevoComentario y el CSS del footer

--- src/components/NuevoComentario/NuevoComentario.jsx
+++ src/components/NuevoComentario/NuevoComentario.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import "./NuevoComentario.css";
@@ -9,7 +9,16 @@ import { addComentario } from "../../redux/comentarios/comentarios.actions";
 const NuevoComentario = ({ eventoId, user }) => {
   const { register, handleSubmit, reset } = useForm();
   const dispatch = useDispatch();
+  const [value, setValue] = useState(0);
 
+  const handleChange = (event) => {
+    const newValue = parseInt(event.target.value);
+
+    if (newValue >= 0 && newValue <= 5) {
+      setValue(newValue);
+    }
+  };
+  
   const onSubmit = (data) => {
     const comentarioData = {
       title: data.titulo,
@@ -36,7 +45,7 @@ const NuevoComentario = ({ eventoId, user }) => {
           </div>
           <div className="form-group">
             <label>Valoración</label>
-            <input type="number" {...register("valoracion")} min={0} max={5} className="inputVal"/>
+            <input type="number" {...register("valoracion")} min={0} max={5} value={value} onChange={handleChange} className="inputVal"/>
           </div>
         </div>
 
